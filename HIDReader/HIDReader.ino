845e9c993dcd0377bfb6d17e93ddff80d0ec47d5
samamorgan <samamorgan@hotmail.com>
2016-02-10 07:48:35 -0800

    Fixed broken keypad logic
    Code wasn't accounting for the character size of * and # (2 characters instead of one), and guesses weren't resetting properly on auth

--- HIDReader/HIDReader.ino
+++ HIDReader/HIDReader.ino
@@ -1,6 +1,6 @@
 /*
  * HID RFID Reader Wiegand Interface for Arduino Uno
- * Written by Sam Morgan, 2015.02.08
+ * Written by Sam Morgan, 2015.02.10
  * Original HID Weigand interface by by Daniel Smith, 2012.01.30
  *                                      www.pagemac.com
  *
@@ -33,16 +33,26 @@
 #define WEIGAND_WAIT_TIME  3000      // time to wait for another wiegand pulse. 
 
 // 4 digit PINs that have been converted to 64-bit SHA-256 hexes
-const char pin1[] PROGMEM = "ac8c1aa79856748c7dfc370cdd0f5d01841c36b8b22eabf69c4f495bf8eba4d7";
-const char pin2[] PROGMEM = "0315b4020af3eccab7706679580ac87a710d82970733b8719e70af9b57e7b9e6";
-const char pin3[] PROGMEM = "2f4011ca31d756ee52aa794fa11f9c1d54f0701969a9462607dcdf6abc8eaed9";
-const char pin4[] PROGMEM = "9106f1ec4a2142f02273d7a820b6fd53c612fdfbdf8626c96d65af38828e735e";
-const char pin5[] PROGMEM = "5c6ab6a10221871a18b25558a77a99d1324732e4d5ac403e0bed5d85acba24fd";
-const char pin6[] PROGMEM = "5a2398a2ea274d788e478bf17845d42aa0c9d5aa9c1415ba01156e8609d27dcd";
-const char pin7[] PROGMEM = "90fe2c25cc8b9530bd60a2b198ce85c53b06521848c81ba9ecb2a7f57e3c06d8";
+const char pin01[] PROGMEM = "0315b4020af3eccab7706679580ac87a710d82970733b8719e70af9b57e7b9e6";
+const char pin02[] PROGMEM = "9106f1ec4a2142f02273d7a820b6fd53c612fdfbdf8626c96d65af38828e735e";
+const char pin03[] PROGMEM = "5a2398a2ea274d788e478bf17845d42aa0c9d5aa9c1415ba01156e8609d27dcd";
+const char pin04[] PROGMEM = "90fe2c25cc8b9530bd60a2b198ce85c53b06521848c81ba9ecb2a7f57e3c06d8";
+const char pin05[] PROGMEM = "937554b0f5ce7ea8254bfcdfc6f6133841ab7c416ceaa1f5de86b7dbec1b24d9";
+const char pin06[] PROGMEM = "196499f197648f9eb37ffafe41ba444d531d59784a3bcddd0e80e77bade487d1";
+const char pin07[] PROGMEM = "5c6ab6a10221871a18b25558a77a99d1324732e4d5ac403e0bed5d85acba24fd";
+const char pin08[] PROGMEM = "90b5bc7f03c840b2efddb22ffdfc37dd12cb391b49aa0fc8751726c04d32ff30";
+const char pin09[] PROGMEM = "24fd7e2735af185459f293eb8704789722c8e46ef86c880322577fe019bb829c";
+const char pin10[] PROGMEM = "ac8c1aa79856748c7dfc370cdd0f5d01841c36b8b22eabf69c4f495bf8eba4d7";
+const char pin11[] PROGMEM = "cbfad02f9ed2a8d1e08d8f74f5303e9eb93637d47f82ab6f1c15871cf8dd0481";
+const char pin12[] PROGMEM = "286c897eba57ce67e79fff80229d9eacddde784b29a18a1d47c17bade5ad1a08";
+const char pin13[] PROGMEM = "1c49f22f6de9bd15e5e566fa8983be4cfa4709abf0f95edf96dcd3d6249c2649";
+const char pin14[] PROGMEM = "2f4011ca31d756ee52aa794fa11f9c1d54f0701969a9462607dcdf6abc8eaed9";
+const char pin15[] PROGMEM = "eb5e19340837ae6d45ab28fa05cb781e34520a3cf8d0b996d3a3875bd50863b9";
+
+
 
 // Table to point at PROGMEM PIN hashes
-const char* const string_table[] PROGMEM = {pin1, pin2, pin3, pin4, pin5, pin6, pin7};
+const char* const string_table[] PROGMEM = {pin01, pin02, pin03, pin04, pin05, pin06, pin07, pin08, pin09, pin10, pin11, pin12, pin13, pin14, pin15};
 
 char buffer[64];
 
@@ -159,7 +169,11 @@ void loop() {
          pin |= databits[i];
       }
       guess = guess + String(pin);
-      if (guess.length() == 4) {
+      if (pin == 10 || pin == 11) {
+        guess = "";
+        secure(2);
+      }
+      if (guess.length() >= 4) {
         tries++;
         if(openDoor(checkPin(guess))) {
           tries = 0;
